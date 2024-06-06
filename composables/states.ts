a67fc933fb1ed586909d86b96a94667c239f47ee
ShervonnW <brybussiw@gmail.com>
2024-06-06 14:51:58 -0400

    added files

---
+++ composables/states.ts
@@ -0,0 +1,5 @@
+import { type IMessage } from "~/interfaces/IMessage";
+
+
+export const useIsChatting = () => useState("isChatting", () => false);
+export const useMessages =() => useState<IMessage[]>("messages", () => []);
---
+++ server/utils/openai.ts
@@ -0,0 +1,7 @@
+import OpenAI from "openai";
+
+export const client = new OpenAI({
+    apiKey: "sk-proj-Gu7nD4t7DirpjuAAXVg8T3BlbkFJY5GU6yrn1MKkNjHOysiI",
+});
+
+export const assistant = "asst_FCDBjhlcuDFG5vXan98j32ka";
